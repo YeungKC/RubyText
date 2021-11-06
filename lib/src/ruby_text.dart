a9bf65ddfb56244678fe185f4f0bc11d52f43aac
YeungKC <I@YeungKC.com>
2021-11-07 06:00:04 +0800

    remove functional_widget

--- lib/src/ruby_text.dart
+++ lib/src/ruby_text.dart
@@ -1,143 +1,159 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 import 'package:tuple/tuple.dart';
 
 import 'ruby_text_data.dart';
 
-part 'ruby_text.g.dart';
+typedef _BuildRubySpanResult = Tuple2<TextStyle, TextStyle>;
 
-typedef BuildRubySpanResult = Tuple2<TextStyle, TextStyle>;
+class RubySpanWidget extends HookWidget {
+  const RubySpanWidget(this.data, {Key? key}) : super(key: key);
 
-@hwidget
-Widget rubySpanWidget(BuildContext context, RubyTextData data) {
-  final defaultTextStyle = DefaultTextStyle.of(context).style;
-  final boldTextOverride = MediaQuery.boldTextOverride(context);
+  final RubyTextData data;
 
-  final result = useMemoized(
-    () {
-      // text style
-      var effectiveTextStyle = data.style;
-      if (effectiveTextStyle == null || effectiveTextStyle.inherit) {
-        effectiveTextStyle = defaultTextStyle.merge(effectiveTextStyle);
-      }
-      if (boldTextOverride) {
-        effectiveTextStyle = effectiveTextStyle
-            .merge(const TextStyle(fontWeight: FontWeight.bold));
-      }
-      assert(effectiveTextStyle.fontSize != null, 'must be has a font size.');
-      final defaultRubyTextStyle = effectiveTextStyle.merge(
-        TextStyle(fontSize: effectiveTextStyle.fontSize! / 1.5),
-      );
+  @override
+  Widget build(BuildContext context) {
+    final defaultTextStyle = DefaultTextStyle.of(context).style;
+    final boldTextOverride = MediaQuery.boldTextOverride(context);
 
-      // ruby text style
-      var effectiveRubyTextStyle = data.rubyStyle;
-      if (effectiveRubyTextStyle == null || effectiveRubyTextStyle.inherit) {
-        effectiveRubyTextStyle =
-            defaultRubyTextStyle.merge(effectiveRubyTextStyle);
-      }
-      if (boldTextOverride) {
-        effectiveRubyTextStyle = effectiveRubyTextStyle
-            .merge(const TextStyle(fontWeight: FontWeight.bold));
-      }
-
-      // spacing
-      final ruby = data.ruby;
-      final text = data.text;
-      if (ruby != null &&
-          effectiveTextStyle.letterSpacing == null &&
-          effectiveRubyTextStyle.letterSpacing == null &&
-          ruby.length >= 2 &&
-          text.length >= 2) {
-        final rubyWidth = _measurementWidth(
-          ruby,
-          effectiveRubyTextStyle,
-          textDirection: data.textDirection,
-        );
-        final textWidth = _measurementWidth(
-          text,
-          effectiveTextStyle,
-          textDirection: data.textDirection,
-        );
-
-        if (textWidth > rubyWidth) {
-          final newLetterSpacing = (textWidth - rubyWidth) / ruby.length;
-          effectiveRubyTextStyle = effectiveRubyTextStyle
-              .merge(TextStyle(letterSpacing: newLetterSpacing));
-        } else {
-          final newLetterSpacing = (rubyWidth - textWidth) / text.length;
-          effectiveTextStyle = effectiveTextStyle
-              .merge(TextStyle(letterSpacing: newLetterSpacing));
+    final result = useMemoized(
+      () {
+        // text style
+        var effectiveTextStyle = data.style;
+        if (effectiveTextStyle == null || effectiveTextStyle.inherit) {
+          effectiveTextStyle = defaultTextStyle.merge(effectiveTextStyle);
         }
-      }
+        if (boldTextOverride) {
+          effectiveTextStyle = effectiveTextStyle
+              .merge(const TextStyle(fontWeight: FontWeight.bold));
+        }
+        assert(effectiveTextStyle.fontSize != null, 'must be has a font size.');
+        final defaultRubyTextStyle = effectiveTextStyle.merge(
+          TextStyle(fontSize: effectiveTextStyle.fontSize! / 1.5),
+        );
 
-      return BuildRubySpanResult(effectiveTextStyle, effectiveRubyTextStyle);
-    },
-    [defaultTextStyle, boldTextOverride, data],
-  );
+        // ruby text style
+        var effectiveRubyTextStyle = data.rubyStyle;
+        if (effectiveRubyTextStyle == null || effectiveRubyTextStyle.inherit) {
+          effectiveRubyTextStyle =
+              defaultRubyTextStyle.merge(effectiveRubyTextStyle);
+        }
+        if (boldTextOverride) {
+          effectiveRubyTextStyle = effectiveRubyTextStyle
+              .merge(const TextStyle(fontWeight: FontWeight.bold));
+        }
 
-  final effectiveTextStyle = result.item1;
-  final effectiveRubyTextStyle = result.item2;
+        // spacing
+        final ruby = data.ruby;
+        final text = data.text;
+        if (ruby != null &&
+            effectiveTextStyle.letterSpacing == null &&
+            effectiveRubyTextStyle.letterSpacing == null &&
+            ruby.length >= 2 &&
+            text.length >= 2) {
+          final rubyWidth = _measurementWidth(
+            ruby,
+            effectiveRubyTextStyle,
+            textDirection: data.textDirection,
+          );
+          final textWidth = _measurementWidth(
+            text,
+            effectiveTextStyle,
+            textDirection: data.textDirection,
+          );
 
-  final texts = <Widget>[];
-  if (data.ruby != null) {
+          if (textWidth > rubyWidth) {
+            final newLetterSpacing = (textWidth - rubyWidth) / ruby.length;
+            effectiveRubyTextStyle = effectiveRubyTextStyle
+                .merge(TextStyle(letterSpacing: newLetterSpacing));
+          } else {
+            final newLetterSpacing = (rubyWidth - textWidth) / text.length;
+            effectiveTextStyle = effectiveTextStyle
+                .merge(TextStyle(letterSpacing: newLetterSpacing));
+          }
+        }
+
+        return _BuildRubySpanResult(effectiveTextStyle, effectiveRubyTextStyle);
+      },
+      [defaultTextStyle, boldTextOverride, data],
+    );
+
+    final effectiveTextStyle = result.item1;
+    final effectiveRubyTextStyle = result.item2;
+
+    final texts = <Widget>[];
+    if (data.ruby != null) {
+      texts.add(
+        Text(
+          data.ruby!,
+          textAlign: TextAlign.center,
+          style: effectiveRubyTextStyle,
+        ),
+      );
+    }
+
     texts.add(
       Text(
-        data.ruby!,
+        data.text,
         textAlign: TextAlign.center,
-        style: effectiveRubyTextStyle,
+        style: effectiveTextStyle,
       ),
+    );
+
+    return Column(
+      children: texts,
     );
   }
-
-  texts.add(
-    Text(
-      data.text,
-      textAlign: TextAlign.center,
-      style: effectiveTextStyle,
-    ),
-  );
-
-  return Column(
-    children: texts,
-  );
 }
 
-@swidget
-Widget rubyText(
-  BuildContext context,
-  List<RubyTextData> data, {
-  double spacing = 0.0,
-  TextStyle? style,
-  TextStyle? rubyStyle,
-  TextAlign? textAlign,
-  TextDirection? textDirection,
-  bool? softWrap,
-  TextOverflow? overflow,
-  int? maxLines,
-}) =>
-    Text.rich(
-      TextSpan(
-        children: data
-            .map<InlineSpan>(
-              (RubyTextData data) => WidgetSpan(
-                child: RubySpanWidget(
-                  data.copyWith(
-                    style: style,
-                    rubyStyle: rubyStyle,
-                    textDirection: textDirection,
+class RubyText extends StatelessWidget {
+  const RubyText(
+    this.data, {
+    Key? key,
+    this.spacing = 0.0,
+    this.style,
+    this.rubyStyle,
+    this.textAlign,
+    this.textDirection,
+    this.softWrap,
+    this.overflow,
+    this.maxLines,
+  }) : super(key: key);
+
+  final List<RubyTextData> data;
+  final double spacing;
+  final TextStyle? style;
+  final TextStyle? rubyStyle;
+  final TextAlign? textAlign;
+  final TextDirection? textDirection;
+  final bool? softWrap;
+  final TextOverflow? overflow;
+  final int? maxLines;
+
+  @override
+  Widget build(BuildContext context) => Text.rich(
+        TextSpan(
+          children: data
+              .map<InlineSpan>(
+                (RubyTextData data) => WidgetSpan(
+                  child: RubySpanWidget(
+                    data.copyWith(
+                      style: style,
+                      rubyStyle: rubyStyle,
+                      textDirection: textDirection,
+                    ),
                   ),
                 ),
-              ),
-            )
-            .toList(),
-      ),
-      textAlign: textAlign,
-      textDirection: textDirection,
-      softWrap: softWrap,
-      overflow: overflow,
-      maxLines: maxLines,
-    );
+              )
+              .toList(),
+        ),
+        textAlign: textAlign,
+        textDirection: textDirection,
+        softWrap: softWrap,
+        overflow: overflow,
+        maxLines: maxLines,
+      );
+}
 
 double _measurementWidth(
   String text,
