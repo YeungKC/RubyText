1c0ef3e68d7a6b2c38d181584ed17f2fd1d94593
Jiakuan(Daniel) Li <jiakuan.li.cs@gmail.com>
2022-10-22 11:15:14 +0900

    able to toggle hiragana on and off

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,142 +10,171 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(64),
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: RubyText(
-                      [
-                        RubyTextData(
-                          'でいごの',
-                        ),
-                        RubyTextData(
-                          '花',
-                          ruby: 'はな',
-                        ),
-                        RubyTextData(
-                          'が',
-                        ),
-                        RubyTextData(
-                          '咲',
-                          ruby: 'さ',
-                        ),
-                        RubyTextData(
-                          'き',
-                        ),
-                        RubyTextData(
-                          '、',
-                        ),
-                        RubyTextData(
-                          '風',
-                          ruby: 'かぜ',
-                        ),
-                        RubyTextData(
-                          'を',
-                        ),
-                        RubyTextData(
-                          '呼',
-                          ruby: 'よ',
-                        ),
-                        RubyTextData(
-                          'び',
-                        ),
-                        RubyTextData(
-                          '、',
-                        ),
-                        RubyTextData(
-                          '嵐',
-                          ruby: 'あらし',
-                        ),
-                        RubyTextData(
-                          'が',
-                        ),
-                        RubyTextData(
-                          '来',
-                          ruby: 'き',
-                        ),
-                        RubyTextData(
-                          'た',
-                        ),
-                        RubyTextData(
-                          '呼',
-                          ruby: 'よ',
-                        ),
-                        RubyTextData(
-                          '、',
-                        ),
-                        RubyTextData(
-                          'でいごが',
-                        ),
-                        RubyTextData(
-                          '咲',
-                          ruby: 'さ',
-                        ),
-                        RubyTextData(
-                          'き',
-                        ),
-                        RubyTextData(
-                          '乱',
-                          ruby: 'みだ',
-                        ),
-                        RubyTextData(
-                          'れ',
-                        ),
-                        RubyTextData(
-                          '、',
-                        ),
-                        RubyTextData(
-                          '嵐',
-                          ruby: 'あらし',
-                        ),
-                        RubyTextData(
-                          'が',
-                        ),
-                        RubyTextData(
-                          '来',
-                          ruby: 'き',
-                        ),
-                        RubyTextData(
-                          'た。',
-                        ),
-                      ],
-                    ),
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool showHiraganas = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(64),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: _buildRubyText(showHiraganas),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: RubyText(
+                    [
+                      RubyTextData(
+                        '検査',
+                        ruby: 'けんさけんさ',
+                      ),
+                    ],
+                    showHiraganas: showHiraganas,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: RubyText(
-                      [
-                        RubyTextData(
-                          '検査',
-                          ruby: 'けんさけんさ',
-                        ),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: RubyText(
+                    [
+                      RubyTextData(
+                        '検査検査',
+                        ruby: 'けんさ',
+                      ),
+                    ],
+                    showHiraganas: showHiraganas,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: RubyText(
-                      [
-                        RubyTextData(
-                          '検査検査',
-                          ruby: 'けんさ',
-                        ),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        showHiraganas = !showHiraganas;
+                      });
+                    },
+                    child: Text('Toggle Hiraganas'),
                   ),
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildRubyText(bool showHiraganas) {
+    return RubyText(
+      [
+        RubyTextData(
+          'でいごの',
+        ),
+        RubyTextData(
+          '花',
+          ruby: 'はな',
+        ),
+        RubyTextData(
+          'が',
+        ),
+        RubyTextData(
+          '咲',
+          ruby: 'さ',
+        ),
+        RubyTextData(
+          'き',
+        ),
+        RubyTextData(
+          '、',
+        ),
+        RubyTextData(
+          '風',
+          ruby: 'かぜ',
+        ),
+        RubyTextData(
+          'を',
+        ),
+        RubyTextData(
+          '呼',
+          ruby: 'よ',
+        ),
+        RubyTextData(
+          'び',
+        ),
+        RubyTextData(
+          '、',
+        ),
+        RubyTextData(
+          '嵐',
+          ruby: 'あらし',
+        ),
+        RubyTextData(
+          'が',
+        ),
+        RubyTextData(
+          '来',
+          ruby: 'き',
+        ),
+        RubyTextData(
+          'た',
+        ),
+        RubyTextData(
+          '呼',
+          ruby: 'よ',
+        ),
+        RubyTextData(
+          '、',
+        ),
+        RubyTextData(
+          'でいごが',
+        ),
+        RubyTextData(
+          '咲',
+          ruby: 'さ',
+        ),
+        RubyTextData(
+          'き',
+        ),
+        RubyTextData(
+          '乱',
+          ruby: 'みだ',
+        ),
+        RubyTextData(
+          'れ',
+        ),
+        RubyTextData(
+          '、',
+        ),
+        RubyTextData(
+          '嵐',
+          ruby: 'あらし',
+        ),
+        RubyTextData(
+          'が',
+        ),
+        RubyTextData(
+          '来',
+          ruby: 'き',
+        ),
+        RubyTextData(
+          'た。',
+        ),
+      ],
+      showHiraganas: showHiraganas,
     );
   }
 }
